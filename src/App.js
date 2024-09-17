6c9dace60de4dd5bd4df581039ab14db1037f646
slime-k <metarusuraimuhagure@gmail.com>
2024-09-17 11:03:15 +0900

    頼む！

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React5
+          Learn React山﨑氏
         </a>
       </header>
     </div>
